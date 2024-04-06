75334332d743e33cf77e67537a345651faca11b0
vortep.7 <0ndreyp7777@gmail.com>
2024-04-06 12:51:21 +0300

    create coordinator flow: interface + app coord

---
+++ WorkTracker/Core/AuthCoordinatorProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  AuthCoordinatorProtocol.swift
+//  WorkTracker
+//
+//  Created by Андрей Петров on 06.04.2024.
+//
+
+import Foundation
---
+++ WorkTracker/Core/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  WorkTracker
+//
+//  Created by Андрей Петров on 06.04.2024.
+//
+
+import Foundation
